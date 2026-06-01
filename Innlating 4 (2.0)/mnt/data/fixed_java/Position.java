0ce32f9db818214e08f3468836839ef69670ba8f
opfino <Erik.Hammer05@gmail.com>
2026-06-01 20:17:54 +0100

    Delete Innlating 4 (2.0) directory

--- Innlating 4 (2.0)/mnt/data/fixed_java/Position.java
+++
@@ -1,9 +0,0 @@
-public class Position {
-    public int x; // column, 0-based
-    public int y; // row, 0-based
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
